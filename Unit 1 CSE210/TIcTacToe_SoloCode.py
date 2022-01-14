81cbdc55a6b5be677c10fcae6d614578c1303063
Hilmar Orozco <oro18005@byui.edu>
2022-01-13 17:04:01 -0700

    Co-authored-by: Emma Burkett <EmmaBurkett@users.noreply.github.com>

--- Unit 1 CSE210/TIcTacToe_SoloCode.py
+++ Unit 1 CSE210/TIcTacToe_SoloCode.py
@@ -1,66 +1,61 @@
 '''
-Solo Checkpoint 02
-Author: Bro. Hayes
+Tic-Tac-Toe: A Solution
+Author: Hilmar Orozco L
+
+from termcolor import colored
 '''
-​
 def main():
-    ''' Holds the main game loop logic
-        Selects a player
-        Builds the board
-        Loops through Players until a winner is found or game is over
-        Displays results to user
-        Thanks them for playing
-        return: None
-    '''
-    # assign/get the first player
-​
-    # create a board
-​
-    # loop if there isn't a winner or if the game isn't a draw
-​
-        # display the board
-​
-        # allow the player to make a move
-​
-        # pick the next player
-​
-    # display the final board
-​
-    # show message for winner and thanks for playing
-    pass
-​
+    player = next_player("")
+    board = create_board()
+    while not (has_winner(board) or is_a_draw(board)):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print("Good game. Thanks for playing!") 
+
 def create_board():
-    ''' Creates a list that holds the spots on the board
-        It initializes the positions with the numbers for the person to pick
-        return: the board as a list
-    '''
-    pass
-​
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
+
 def display_board(board):
-    ''' Displays the current board
-        return: None
-    '''
-    pass
-​
-def is_draw(board):
-    ''' return: True if board is a draw, False if board is still playable '''
-    pass
-​
-def is_winner(board):
-    ''' return: True if someone won, False if there is no winner '''
-    pass
-​
+    #print(f"{_colored_charboard[0]}|{board[1]}|{board[2]}")
+    
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+    
+def is_a_draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True 
+    
+def has_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
+
 def make_move(player, board):
-    ''' Prompts player to select a square to play
-        Assigns the player to that board location if it is a legal move
-        return: None
-    '''
-    pass      
-​
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
+
 def next_player(current):
-    ''' return: x if current is o, otherwise x '''
-    pass
-​
-# run main if this has been called from the command line
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
 if __name__ == "__main__":
     main()
